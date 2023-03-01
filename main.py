aebfba5cc6ab9c2ee381e2a560839395b03a48ed
Phoorin Nickphong <6510110371@psu.ac.th>
2023-03-01 23:55:01 +0700

    Aapp window size

---
+++ main.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+
+
+class Game(Widget):
+    def __init__(self, **kwargs):
+        super(Game, self).__init__(**kwargs)
+
+        self.win_w = Window.size[0]
+        self.win_h = Window.size[1]
+
+        ''' Sizes '''
+        self.player_size = (self.win_w * .06, self.win_h * .08)
+        self.fire_size = (self.player_size[0] / 10, self.player_size[1] / 2)
+        self.enemy_size = (self.win_w * .06, self.win_h * .05)
+        self.life_size = (self.win_w * .035, self.win_h * .035)
+
+class MainApp(App):
+    def build(self):
+        self.window_size = Window.size
+
+if __name__ == '__main__':
+    MainApp().run()
